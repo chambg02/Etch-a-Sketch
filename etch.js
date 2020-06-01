894976dc81dcb02ca5b371b3dddba6f9b1d1bf0e
Geoff Chambon <chambg02@gmail.com>
2020-06-02 04:48:11 +0800

    Just need to figure out how to make the squares on the next line.

--- etch.js
+++ etch.js
@@ -1,16 +1,36 @@
 
-for (i=0;i<17;i++) {
-const box = document.createElement('div');
-box.style.border = "1px solid";
-box.style.width = "50px";
-box.style.height = "50px";
+function createRow() {
+	for (a=0;a<17;a++) {
+	const row = document.createElement('div');
+	row.style.border = "1px solid";
+	row.style.width = "50px";
+	row.style.height = "50px";
+	row.style.cssFloat = "left";
 
-box.onclick= () => box.style.background="yellow";
+	row.onclick= () => row.style.background="yellow";
+
+container.appendChild(row);
+}
+}
+
+function lineBreak() {
+	const lb = document.createElement("br");
+	row.appendChild(lb);
+}
+
+for (i=0;i<17;i++) {
+createRow();
+;
+}
+
+
+
+
+
 
 //container.querySelector('#box1');
 //box1.addEventListener('hover', () => {
 //	box1.style.background = "yellow";
 //});
 
-container.appendChild(box);
-}
+
