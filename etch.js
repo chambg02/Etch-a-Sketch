00176d7138035748bac6b69db352bf61e5bb7437
Geoff Chambon <chambg02@gmail.com>
2020-06-04 20:41:57 +0800

    now worked out how to make rows/columns

--- etch.js
+++ etch.js
@@ -1,36 +1,16 @@
+function createBlock() {
+	const block = document.createElement('div');
+	block.style.border = "1px solid";
+	block.style.width = "50px";
+	block.style.height = "50px";
+	block.style.cssFloat = "left"; //makes each new div in a horizontal line
 
-function createRow() {
-	for (a=0;a<17;a++) {
-	const row = document.createElement('div');
-	row.style.border = "1px solid";
-	row.style.width = "50px";
-	row.style.height = "50px";
-	row.style.cssFloat = "left";
+	block.onmouseover= () => block.style.background="yellow"; //colour the blocks on hover
 
-	row.onclick= () => row.style.background="yellow";
-
-container.appendChild(row);
-}
-}
-
-function lineBreak() {
-	const lb = document.createElement("br");
-	row.appendChild(lb);
+container.appendChild(block); 
 }
 
 for (i=0;i<17;i++) {
-createRow();
-;
+createBlock();
+
 }
-
-
-
-
-
-
-//container.querySelector('#box1');
-//box1.addEventListener('hover', () => {
-//	box1.style.background = "yellow";
-//});
-
-
