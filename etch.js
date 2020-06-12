0ca71bd4d1cccba553b0607429caf41661dbd063
Geoff Chambon <chambg02@gmail.com>
2020-06-12 22:11:09 +0800

    project complete to requirements

--- etch.js
+++ etch.js
@@ -1,16 +1,29 @@
+var userSize; //to use a variable across functions, define it first outside function.
+var blockSize;
+var convertedSize;
+
+function size() {
+	userSize = prompt("Enter number of columns to make a square");
+	convertedSize = +userSize + +1;
+	//call variable without "let or var"
+}
+
+size();
+
 function createBlock() {
+	blockSize = 960/convertedSize;
 	const block = document.createElement('div');
-	block.style.border = "1px solid";
-	block.style.width = "50px";
-	block.style.height = "50px";
+	block.style.width = blockSize + "px"; //set size using variable blockSize
+	block.style.height = blockSize + "px"; //set size using variable blockSize
 	block.style.cssFloat = "left"; //makes each new div in a horizontal line
 
-	block.onmouseover= () => block.style.background="yellow"; //colour the blocks on hover
+	block.onmouseover= () => block.style.background="black"; //colour the blocks on hover
 
 container.appendChild(block); 
 }
 
-for (i=0;i<17;i++) {
+for (i=0;i<userSize*userSize;i++) {
 createBlock();
-
 }
+
+
